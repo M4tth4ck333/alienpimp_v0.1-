26521c541a3329f1de68f2d2e186352637668959
m4tt <jjschroder63@gmail.com>
2025-06-22 13:46:49 +0200

    Update setup.py
    sexyer

--- setup.py
+++ setup.py
@@ -10,22 +10,13 @@ with open("README.md", "r", encoding="utf-8") as fh:
 setup(
     name="alienpimp",
     version="0.1.0",
-    
-    # --- BITTE HIER DEINE DATEN EINTRAGEN ---
-    author="Dein Name",  # <-- HIER ÄNDERN
-    author_email="deine@email.de",  # <-- HIER ÄNDERN
-    url="https://github.com/DEINUSERNAME/alienpimp",  # <-- HIER DEINEN GITHUB-USERNAME EINTRAGEN
-    
-    # --- BESCHREIBUNG DES PROJEKTS ---
-    # Die Beschreibung ist gut, aber überlege, ob du "JAN-Integration" für andere
-    # verständlicher machen kannst, z.B. im README.
+    author="Jan Schroeder",  
+    author_email="jjschroeder64@gmail.con",  
+    url="https://github.com/M4tth4ck333/alienpimp",
     description="Modulares Pentest-Toolkit mit TUI, Datenbank und KI-Integration",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    
     license="MIT",
-    
-    # --- PAKETE UND ABHÄNGIGKEITEN ---
     packages=find_packages(),
     python_requires=">=3.8",
     install_requires=[
@@ -34,21 +25,16 @@ setup(
         "numpy>=1.21",
         "torch>=2.0",
         "requests>=2.22.0",
-        "rich",           # Für schöne CLI-Ausgaben
-        "prompt_toolkit", # Für TUI/CLI-Interaktivität
+        "rich",           
+        "prompt_toolkit", 
         # ggf. weitere Abhängigkeiten hier eintragen
     ],
     
-    # Macht dein Tool direkt in der Kommandozeile verfügbar
-    # Wenn ein Benutzer `pip install .` ausführt und dann `alienpimp` tippt,
-    # wird die Funktion `main` in der Datei `alienpimp/tui.py` gestartet.
     entry_points={
         "console_scripts": [
             "alienpimp=alienpimp.tui:main"
         ]
     },
-    
-    # --- METADATEN FÜR PYPI (Python Package Index) ---
     # Diese Klassifizierer sind sehr gut gewählt!
     classifiers=[
         "Programming Language :: Python :: 3",
